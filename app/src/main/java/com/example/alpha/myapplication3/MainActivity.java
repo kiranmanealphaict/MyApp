e5b5bf3324f75471bebbda70c26b66cd89197f3a
alpha <kiranmanealphaict@gmail.com>
2017-06-07 17:13:00 +0530

    4 commit

--- app/src/main/java/com/example/alpha/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/alpha/myapplication3/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-
+        System.out.println("Hello");
         System.out.println("Hello");
     }
 
