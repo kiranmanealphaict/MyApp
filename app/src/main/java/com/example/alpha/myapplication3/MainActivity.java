89af35b6a75b7a9025cbc4c3ecc8472ac0f89b90
alpha <kiranmanealphaict@gmail.com>
2017-06-08 10:34:57 +0530

    add sysout kiran

--- app/src/main/java/com/example/alpha/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/alpha/myapplication3/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity
 
         System.out.println("Hello");
         System.out.println("Hello");
-        
+
 
         System.out.println("Hello");
 
